d12464976f2c31c773680437a9c960f1e61697a6
wihn <wihn2021@163.com>
2022-10-29 23:46:04 +0800

    build: add some 'extern's, create new files

---
+++ CAPI/API/src/client.cpp
@@ -0,0 +1,9 @@
+#include "AI.h"
+#include "API.h"
+extern const THUAI6::PlayerType playerType;
+extern const THUAI6::ButcherType butcherType;
+extern const THUAI6::HumanType humanType;
+int main()
+{
+    
+}

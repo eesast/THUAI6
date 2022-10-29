874207c11054886c646843519e0c3f87d9971fa1
DragonAura <dragonaurapvp@gmail.com>
2022-10-29 23:20:58 +0800

    refactor: :art: move services to Services.proto
    将服务另开了一个proto文件进行定义

--- CAPI/proto/Message2Clients.pb.h
+++ CAPI/proto/Message2Clients.pb.h
@@ -32,7 +32,6 @@
 #include <google/protobuf/extension_set.h>   // IWYU pragma: export
 #include <google/protobuf/unknown_field_set.h>
 #include "MessageType.pb.h"
-#include "Message2Server.pb.h"
 // @@protoc_insertion_point(includes)
 #include <google/protobuf/port_def.inc>
 #define PROTOBUF_INTERNAL_EXPORT_Message2Clients_2eproto

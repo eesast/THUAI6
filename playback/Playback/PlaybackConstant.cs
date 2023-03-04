5c63fade5314969e07185713312735c13048e171
ysqTHU <108213422+ysqTHU@users.noreply.github.com>
2023-03-04 22:29:00 +0800

    debug

--- playback/Playback/PlaybackConstant.cs
+++ playback/Playback/PlaybackConstant.cs
@@ -3,6 +3,6 @@ namespace Playback
     public static class PlayBackConstant
     {
         public static string ExtendedName = ".thuaipb";
-        public static byte[] Prefix = { (byte)'P', (byte)'B', 5, 0 };   // 文件前缀，用于标识文件类型，版本号为6
+        public static byte[] Prefix = { (byte)'P', (byte)'B', 6, 0 };   // 文件前缀，用于标识文件类型，版本号为6
     }
 }

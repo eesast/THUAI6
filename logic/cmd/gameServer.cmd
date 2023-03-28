5b5ba70d0272ca8e7a6fb576749f2c12ab73e2e6
Shawqeem <1004837646@qq.com>
2023-03-28 18:08:48 +0800

    chore: :ambulance: commit for merge
    commit for merge

--- logic/cmd/gameServer.cmd
+++ logic/cmd/gameServer.cmd
@@ -1,10 +1,6 @@
 @echo off
 
-<<<<<<< HEAD
-start cmd /k ..\Server\bin\Debug\net6.0\Server.exe --ip 0.0.0.0 --port 8888 --teamCount 2 --playerNum 2 --gameTimeInSecond 60 --fileName test 
-=======
-start cmd /k ..\Server\bin\Debug\net6.0\Server.exe --port 8888 --studentCount 2 --trickerCount 0 --gameTimeInSecond 600 --fileName test 
->>>>>>> 9f14fecb6a4d2b8da8bd9c0db2dfdd4c4608a288
+start cmd /k ..\Server\bin\Debug\net6.0\Server.exe --port 8888 --studentCount 1 --trickerCount 1 --gameTimeInSecond 600 --fileName test 
 
 ping -n 2 127.0.0.1 > NUL
 
@@ -12,4 +8,4 @@ ping -n 2 127.0.0.1 > NUL
 
 ping -n 2 127.0.0.1 > NUL
 
-start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --cl --port=8888 --characterID=1 --type=1 --occupation=1
+start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --cl --port=8888 --characterID=0 --type=1 --occupation=1

fe7930d06d8f62a4919cf025e2e8e6bc91eabd62
OctaAcid <zhuangjiminzuoye@126.com>
2023-04-19 22:22:56 +0800

    chore: removed secretId from code

--- installer/InstallerUpdater/Program.cs
+++ installer/InstallerUpdater/Program.cs
@@ -57,8 +57,8 @@ namespace Program
                                       .Build();           // 创建 CosXmlConfig 对象
 
             // 永久密钥访问凭证
-            string secretId = "AKIDVpzdDcmhS0KNAj5FLG0WnpcBconY9UYN"; //"云 API 密钥 SecretId";
-            string secretKey = "f2FDyVnpgNr67urZ3HF4f5KL8i2DCzY2"; //"云 API 密钥 SecretKey";
+            string secretId = "***"; //"云 API 密钥 SecretId";
+            string secretKey = "***"; //"云 API 密钥 SecretKey";
 
             long durationSecond = 1000;  // 每次请求签名有效时长，单位为秒
             QCloudCredentialProvider cosCredentialProvider = new DefaultQCloudCredentialProvider(

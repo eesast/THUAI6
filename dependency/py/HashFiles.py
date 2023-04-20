cfdc0881f35d3479c4468218ce44995a84f9407c
shangfengh <3495281661@qq.com>
2023-04-21 00:23:03 +0800

    fix: :green_heart: fix CI

--- dependency/py/HashFiles.py
+++ dependency/py/HashFiles.py
@@ -14,12 +14,16 @@ def generateHashFile():
     for root, dirs, files in os.walk(file):
         for file in files:
             path = os.path.join(root, file)
-            with open(path, 'rb') as f:
+            if mimetypes.guess_type(file)[0] == 'text/plain':
+                with open(path, 'r') as f:
                     data = f.read()
-                    if data.find('\r\n') != -1 :
-                        hashlist[path.replace('\\','/')]=hashlib.md5(data.replace('\n','\r\n')).hexdigest()
-                    else :
-                        hashlist[path.replace('\\','/')]=hashlib.md5(data).hexdigest()
+                    if data.find('\r\n') == -1 :
+                        data.replace('\n','\r\n')
+                with open(path, 'w') as f:
+                    f.write(data)
+            with open(path, 'rb') as f:
+                data = f.read()
+                hashlist[path.replace('\\','/')]=hashlib.md5(data).hexdigest()
     targetFile=os.path.join(targetfolder,'hash.json')
     with open(targetFile, 'w') as fp:
         json.dump(hashlist,fp)

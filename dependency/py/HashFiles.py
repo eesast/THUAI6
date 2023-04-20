82f8c0684d584b1ab350eea1b76e189293a584e2
shangfengh <3495281661@qq.com>
2023-04-20 22:06:33 +0800

    feat: :green_heart: Build CI

---
+++ dependency/py/HashFiles.py
@@ -0,0 +1,29 @@
+# -*- coding=utf-8
+import sys
+import os
+import argparse
+import getopt
+import hashlib
+import json
+
+def generateHashFile():
+    file = targetfolder
+    hashlist={}
+    for root, dirs, files in os.walk(file):
+        for file in files:
+            path = os.path.join(root, file)
+            with open(path, 'rb') as f:
+                data = f.read()
+                hashlist[path.replace('\\','/')]=hashlib.md5(data).hexdigest()
+    targetFile=os.path.join(targetfolder,'hash.json')
+    with open(targetFile, 'w') as fp:
+        json.dump(hashlist,fp)
+    
+parser = argparse.ArgumentParser()
+parser.add_argument('-t',type=str)
+
+args=parser.parse_args()
+
+targetfolder = args.t
+
+generateHashFile()

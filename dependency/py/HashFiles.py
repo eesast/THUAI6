c6be42b8b3b0ec360bad6f5bcae78d25ee3dc683
shangfengh <3495281661@qq.com>
2023-04-21 01:28:03 +0800

    fix: :bug: fix CI

--- dependency/py/HashFiles.py
+++ dependency/py/HashFiles.py
@@ -14,13 +14,6 @@ def generateHashFile():
     for root, dirs, files in os.walk(file):
         for file in files:
             path = os.path.join(root, file)
-            if mimetypes.guess_type(file)[0] == 'text/plain':
-                with open(path, 'r', errors='ignore') as f:
-                    data = f.read()
-                    if data.find('\r\n') == -1 :
-                        data.replace('\n','\r\n')
-                with open(path, 'w') as f:
-                    f.write(data)
             with open(path, 'rb') as f:
                 data = f.read()
                 hashlist[path.replace('\\','/')]=hashlib.md5(data).hexdigest()

9c0df4d861d2d90e2dadc3cef1ce40ff9f02dad8
shangfengh <3495281661@qq.com>
2023-04-22 22:10:08 +0800

    docs: :memo: edit 使用文档.md

--- CAPI/shell/RunPython.sh
+++ CAPI/shell/RunPython.sh
@@ -4,3 +4,4 @@ python ./CAPI/python/PyAPI/main.py -I 127.0.0.1 -P 8888 -p 0 -d -o &
 python ./CAPI/python/PyAPI/main.py -I 127.0.0.1 -P 8888 -p 1 -d -o &
 python ./CAPI/python/PyAPI/main.py -I 127.0.0.1 -P 8888 -p 2 -d -o &
 python ./CAPI/python/PyAPI/main.py -I 127.0.0.1 -P 8888 -p 3 -d -o &
+python ./CAPI/python/PyAPI/main.py -I 127.0.0.1 -P 8888 -p 4 -d -o &

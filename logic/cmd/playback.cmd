0b77d514c71ee543bb25dea5d85334c7a86694b8
Shawqeem <1004837646@qq.com>
2023-04-23 20:32:20 +0800

    fix: :bug: fix the bug caused by frequent refresh of map
    fix the bug caused by frequent refresh of map

--- logic/cmd/playback.cmd
+++ logic/cmd/playback.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --playbackFile .\test.thuaipb --playbackSpeed 2
+start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --playbackFile .\longtest.thuaipb --playbackSpeed 4
 
 ping -n 2 127.0.0.1 > NUL

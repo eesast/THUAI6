537e703b4708709cfc08387366904ac58af25758
sendssf <2336526392@qq.com>
2023-04-23 19:22:46 +0800

    Chore: Add runner Dockerfile and some shells

--- dependency/shell/runcpp.sh
+++
@@ -1,6 +0,0 @@
-# /usr/local/PlayerCode
-
-./CAPI/cpp/build/capi -I 127.0.0.1 -P 8888 -p 0 &
-./CAPI/cpp/build/capi -I 127.0.0.1 -P 8888 -p 1 &
-./CAPI/cpp/build/capi -I 127.0.0.1 -P 8888 -p 2 &
-./CAPI/cpp/build/capi -I 127.0.0.1 -P 8888 -p 3 &
--- dependency/shell/runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local
-
-./Server/Server --port 8888 --studentCount 4 --trickerCount 1 --gameTimeInSecond 600

9de01bc0303847014c180647db358a9844d9c1ea
DragonAura <dragonaurapvp@gmail.com>
2023-04-26 18:17:54 +0800

    fix(CAPI): :bug: fix load buffer

--- CAPI/cpp/API/include/logic.h
+++ CAPI/cpp/API/include/logic.h
@@ -148,9 +148,9 @@ private:
     void ProcessMessage();
 
     // 将信息加载到buffer
-    void LoadBufferSelf(const protobuf::MessageToClient& message);
-    void LoadBufferCase(const protobuf::MessageOfObj& item);
-    void LoadBuffer(const protobuf::MessageToClient& message);
+    void LoadBufferSelf(protobuf::MessageToClient message);
+    void LoadBufferCase(protobuf::MessageOfObj item);
+    void LoadBuffer(protobuf::MessageToClient message);
 
     // 解锁AI线程
     void UnBlockAI();

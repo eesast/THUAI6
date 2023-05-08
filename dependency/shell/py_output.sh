8faa06a811bfb39b38f3fb169df7bdb3da5c19d1
DragonAura <dragonaurapvp@gmail.com>
2023-05-08 19:25:36 +0800

    fix: :poop: fix python compile

---
+++ dependency/shell/py_output.sh
@@ -0,0 +1,8 @@
+python3 -m grpc_tools.protoc -I. --python_out=. --pyi_out=. MessageType.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --pyi_out=. Message2Clients.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --pyi_out=. Message2Server.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --pyi_out=. --grpc_python_out=. Services.proto
+chmod -R 755 ./
+mkdir -p ../../CAPI/python/proto
+mv -f ./*.py ../../CAPI/python/proto
+mv -f ./*.pyi ../../CAPI/python/proto

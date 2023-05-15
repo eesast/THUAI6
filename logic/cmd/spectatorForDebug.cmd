e253333cfa5419a44fd96b85ce4180e51be9f7db
Shawqeem <1004837646@qq.com>
2023-05-16 00:57:11 +0800

    fix: :bug: fix the bug that the mapMessage cannot be received correctly when a spectator joins
    fix the bug that the mapMessage cannot be received correctly when a spectator joins.

---
+++ logic/cmd/spectatorForDebug.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+
+start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --port 8888 --characterID 2030
--- logic/cmd/video.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-
-start cmd /k ..\Client\bin\Debug\net6.0-windows\Client.exe --ip  thuai6.eesast.com --port 8889 --characterID 2030

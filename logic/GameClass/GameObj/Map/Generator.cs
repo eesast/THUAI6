287eb4350a8be226f0d0550e20218a43fb9065c6
shangfengh <3495281661@qq.com>
2023-05-20 22:36:21 +0800

    fix: :bug: fix the bug about learning

--- logic/GameClass/GameObj/Map/Generator.cs
+++ logic/GameClass/GameObj/Map/Generator.cs
@@ -37,28 +37,32 @@ namespace GameClass.GameObj
                 lock (gameObjLock)
                     return degreeOfRepair;
             }
-            private set
-            {
-                lock (gameObjLock)
-                {
-                    if (degreeOfRepair < GameData.degreeOfFixedGenerator)//不允许正常破坏已经修好的发电机
-                        if (value < 0) degreeOfRepair = 0;
-                        else degreeOfRepair = value > GameData.degreeOfFixedGenerator ? GameData.degreeOfFixedGenerator : value;
-                }
-            }
         }
 
         public bool Repair(int addDegree, Character character)
         {
-            if (DegreeOfRepair == GameData.degreeOfFixedGenerator) return false;
-            int orgDegreeOfRepair = degreeOfRepair;
-            DegreeOfRepair += addDegree;
-            if (DegreeOfRepair > orgDegreeOfRepair)
-                character.AddScore(GameData.StudentScoreFix(DegreeOfRepair) - GameData.StudentScoreFix(orgDegreeOfRepair));
-            else character.AddScore(GameData.TrickerScoreDamageGenerator(orgDegreeOfRepair) - GameData.TrickerScoreDamageGenerator(DegreeOfRepair));
-            if (DegreeOfRepair == GameData.degreeOfFixedGenerator)
-                return true;
-            else return false;
+            int orgDegreeOfRepair, value;
+            lock (gameObjLock)
+            {
+                if (degreeOfRepair == GameData.degreeOfFixedGenerator) return false;
+                orgDegreeOfRepair = degreeOfRepair;
+
+                degreeOfRepair += addDegree;
+                if (degreeOfRepair < 0) degreeOfRepair = 0;
+                else
+                {
+                    if (degreeOfRepair > GameData.degreeOfFixedGenerator) degreeOfRepair = GameData.degreeOfFixedGenerator;
+                }
+                value = degreeOfRepair;
+            }
+
+            if (value > orgDegreeOfRepair)
+            {
+                character.AddScore(GameData.StudentScoreFix(value) - GameData.StudentScoreFix(orgDegreeOfRepair));
+                if (value == GameData.degreeOfFixedGenerator) return true;
+            }
+            else character.AddScore(GameData.TrickerScoreDamageGenerator(orgDegreeOfRepair) - GameData.TrickerScoreDamageGenerator(value));
+            return false;
         }
     }
 }

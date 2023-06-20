81e480773a078f49908e673c1cb048be65e08327
Timothy Liu <liuxf19@163.com>
2023-06-21 02:55:57 +0800

    chore(shell): exclude players code in format script

--- dependency/shell/format.sh
+++ dependency/shell/format.sh
@@ -4,23 +4,25 @@ cd ../..
 
 for i in {1..3}
 do
-find . -iname "*.c"   \
-   -or -iname "*.h"   \
-   -or -iname "*.C"   \
-   -or -iname "*.H"   \
-   -or -iname "*.cpp" \
-   -or -iname "*.hpp" \
-   -or -iname "*.cc"  \
-   -or -iname "*.hh"  \
-   -or -iname "*.c++" \
-   -or -iname "*.h++" \
-   -or -iname "*.cxx" \
-   -or -iname "*.hxx" \
-   -or -iname "*.i"   \
-   -or -iname "*.ixx" \
-   -or -iname "*.ipp" \
-   -or -iname "*.i++" \
-   | xargs clang-format -i
+find .                               \
+    -not -path "./players/*" -and \( \
+            -iname "*.c"             \
+        -or -iname "*.h"             \
+        -or -iname "*.C"             \
+        -or -iname "*.H"             \
+        -or -iname "*.cpp"           \
+        -or -iname "*.hpp"           \
+        -or -iname "*.cc"            \
+        -or -iname "*.hh"            \
+        -or -iname "*.c++"           \
+        -or -iname "*.h++"           \
+        -or -iname "*.cxx"           \
+        -or -iname "*.hxx"           \
+        -or -iname "*.i"             \
+        -or -iname "*.ixx"           \
+        -or -iname "*.ipp"           \
+        -or -iname "*.i++"           \
+    \) | xargs clang-format -i
 done
 
 pushd logic && dotnet format && popd

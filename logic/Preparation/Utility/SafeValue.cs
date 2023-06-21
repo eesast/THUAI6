df25fc48fe89cee695a5b739c7d5d4adeb169b2c
shangfengh <3495281661@qq.com>
2023-06-21 09:25:49 +0800

    feat: :sparkles: add a new SafaValues, IntWithVariableRange

--- logic/Preparation/Utility/SafeValue.cs
+++ logic/Preparation/Utility/SafeValue.cs
@@ -49,6 +49,10 @@ namespace Preparation.Utility
         public bool And(bool x) => Interlocked.And(ref v, x ? 1 : 0) != 0;
         public bool Or(bool x) => Interlocked.Or(ref v, x ? 1 : 0) != 0;
     }
+    /// <summary>
+    /// 一个能记录Start后完成多少进度的进度条（int），
+    /// 只允许Start时修改needTime，支持TryStop使未完成的进度条清零，支持Set0使进度条强制清零
+    /// </summary>
     public struct IntProgressContinuously
     {
         private long endT = long.MaxValue;
@@ -61,12 +65,22 @@ namespace Preparation.Utility
         public long GetEndTime() => Interlocked.CompareExchange(ref endT, -2, -2);
         public long GetNeedTime() => Interlocked.CompareExchange(ref needT, -2, -2);
         public override string ToString() => "EndTime:" + Interlocked.CompareExchange(ref endT, -2, -2).ToString() + " ms, NeedTime:" + Interlocked.CompareExchange(ref needT, -2, -2).ToString() + " ms";
+        public bool IsFinished()
+        {
+            return Interlocked.CompareExchange(ref endT, -2, -2) <= Environment.TickCount64;
+        }
+        /// <summary>
+        /// GetProgress<0则表明未开始
+        /// </summary>
         public long GetProgress()
         {
             long cutime = Interlocked.CompareExchange(ref endT, -2, -2) - Environment.TickCount64;
             if (cutime <= 0) return Interlocked.CompareExchange(ref needT, -2, -2);
             return Interlocked.CompareExchange(ref needT, -2, -2) - cutime;
         }
+        /// <summary>
+        /// GetProgressDouble<0则表明未开始
+        /// </summary>
         public double GetProgressDouble()
         {
             long cutime = Interlocked.CompareExchange(ref endT, -2, -2) - Environment.TickCount64;
@@ -94,6 +108,92 @@ namespace Preparation.Utility
             if (Environment.TickCount64 < Interlocked.CompareExchange(ref endT, -2, -2))
                 Interlocked.Exchange(ref endT, long.MaxValue);
         }
-        //增加新的写操作可能导致不安全
+        //增加其他新的写操作可能导致不安全
+    }
+    /// <summary>
+    /// 一个保证在[0,maxValue]的可变int，支持可变的maxValue(请确保大于0)
+    /// </summary>
+    public struct IntWithVariableRange
+    {
+        private int v;
+        private int maxV;
+        private readonly object vLock = new();
+        public IntWithVariableRange(int value, int maxValue)
+        {
+            if (maxValue <= 0) Debugger.Output("Bug:IntWithVariableRange.maxValue (" + maxValue.ToString() + ") is less than 0.");
+            v = value < maxValue ? value : maxValue;
+            this.maxV = maxValue;
+        }
+        public override string ToString()
+        {
+            lock (vLock)
+            {
+                return "value:" + v.ToString() + " ,maxValue:" + maxV.ToString();
+            }
+        }
+        public int GetValue() { lock (vLock) return v; }
+        public int GetMaxV() { lock (vLock) return maxV; }
+
+        public void SetMaxV(int maxValue)
+        {
+            lock (vLock)
+            {
+                if (maxValue <= 0) Debugger.Output("Bug:IntWithVariableRange.maxValue (" + maxValue.ToString() + ") is less than 0.");
+                maxV = maxValue;
+                if (v > maxValue) v = maxValue;
+            }
+        }
+        /// <summary>
+        /// 应当保证该value>=0
+        /// </summary>
+        public int SetPositiveV(int value)
+        {
+            lock (vLock)
+            {
+                return v = (value > maxV) ? maxV : value;
+            }
+        }
+        public int SetV(int value)
+        {
+            lock (vLock)
+            {
+                if (value <= 0) return v = 0;
+                return v = (value > maxV) ? maxV : value;
+            }
+        }
+        public int AddV(int addV)
+        {
+            lock (vLock)
+            {
+                v += addV;
+                if (v < 0) return v = 0;
+                if (v > maxV) return v = maxV;
+                return v;
+            }
+        }
+        /// <summary>
+        /// 应当保证该增加值大于0
+        /// </summary>
+        public int AddPositiveV(int addPositiveV)
+        {
+            lock (vLock)
+            {
+                v += addPositiveV;
+                if (v > maxV) return v = maxV;
+                return v;
+            }
+        }
+        /// <summary>
+        /// 应当保证该减少值大于0
+        /// </summary>
+        public int SubPositiveV(int subPositiveV)
+        {
+            lock (vLock)
+            {
+                v += subPositiveV;
+                if (v < 0) return v = 0;
+                return v;
+            }
+        }
     }
 }

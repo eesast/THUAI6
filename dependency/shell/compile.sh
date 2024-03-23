841f1a13342f69594cc6c994625b9bacd01963fc
DragonAura <32223554+DragonAura@users.noreply.github.com>
2024-03-23 22:00:59 +0800

    Update compile.sh

--- dependency/shell/compile.sh
+++ dependency/shell/compile.sh
@@ -1,33 +1,28 @@
 #! /bin/bash 
 # WORKDIR /usr/local/PlayerCode/CAPI/cpp
-i=1
+workdir=/usr/local/PlayerCode/CAPI/cpp
+bind=/usr/local/code
+output=/usr/local/output
+
 flag=1
-bind=/usr/local/mnt
-while (( $i <= 5 ))
-do
-    if [ -f "${bind}/player${i}.cpp" ]; then
-        cp -f $bind/player$i.cpp ./API/src/AI.cpp
-        cmake ./CMakeLists.txt && make -j$(nproc) >compile_log$i.txt 2>&1
-        mv ./capi $bind/capi$i # executable file
-        if [ $? -ne 0 ]; then
-            flag=0
-        fi
-        mv ./compile_log$i.txt $bind/compile_log$i.txt
-    elif [ -f "${bind}/player${i}.py" ]; then
-        pushd ../python
-        cp -f $bind/player$i.py ./PyAPI/AI.py
-        python3 -m compileall ./PyAPI >compile_log$i.txt 2>&1
-        if [ ! -f ./PyAPI/__pycache__/AI.cpython-39.pyc ]; then
-            flag=0
-        else
-            rm -rf ./PyAPI/__pycache__/AI.cpython-39.pyc
-        fi
-        mv ./compile_log$i.txt $bind/compile_log$i.txt
-        popd
+
+cd $bind
+file_count=$(ls -l *.cpp | wc -l);
+if [ $file_count -eq 1 ]
+then
+    filename=$(ls *.cpp)
+    cd $workdir
+    cp -f $bind/$filename $workdir/API/src/AI.cpp
+    cmake ./CMakeLists.txt && make -j$(nproc) >compile_log.txt 2>&1
+    mv ./capi $output/capi
+    if [ $? -eq 0 ]; then
+        flag=0
     fi
-    let "i++"
-done
-# HTML request to update status.
+    mv ./compile_log.txt $output/compile_log.txt
+else
+    flag=0
+fi
+
 if [ $flag -eq 1 ]; then
     curl $URL -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"compile_status":"compiled"}' > $bind/curl_log.txt
 else

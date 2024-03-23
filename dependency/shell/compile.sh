ed8a89297a43a1c92e427b6867186952ffb7433d
DragonAura <32223554+DragonAura@users.noreply.github.com>
2024-03-23 22:08:10 +0800

    Update compile.sh

--- dependency/shell/compile.sh
+++ dependency/shell/compile.sh
@@ -11,10 +11,11 @@ file_count=$(ls -l *.cpp | wc -l);
 if [ $file_count -eq 1 ]
 then
     filename=$(ls *.cpp)
+    base_name=$(basename "$filename" .cpp)
     cd $workdir
     cp -f $bind/$filename $workdir/API/src/AI.cpp
-    cmake ./CMakeLists.txt && make -j$(nproc) >compile_log.txt 2>&1
-    mv ./capi $output/capi
+    cmake ./CMakeLists.txt && make -j$(nproc) >$base_name.txt 2>&1
+    mv ./capi $output/$base_name
     if [ $? -eq 0 ]; then
         flag=0
     fi
